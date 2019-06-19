5703af69244d30f39f44cef2668769cee463dd9b
DB Lee <donlee@microsoft.com>
2019-06-19 18:38:39 +0900

    Update QuorumClient.cs

--- src/TR20.Loyalty.LedgerClient/LedgerClient.Lib/QuorumClient.cs
+++ src/TR20.Loyalty.LedgerClient/LedgerClient.Lib/QuorumClient.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using TR20.Loyalty.LedgerClient.Contracts.EIP20;
+using TR20.Loyalty.LedgerClient.Contracts.EIP20Factory;
+using Nethereum.Web3;
 
-namespace LedgerClient.Lib
+namespace TR20.Loyalty.LedgerClient.Lib
 {
     public class QuorumClient
     {
+
+        EIP20FactoryService erc20FactorySvc;
+        EIP20Service erc20Svc;
+        Web3 web3;
+
         public QuorumClient(string connectionString)
         {
-
+            web3 = new Web3();
         }
 
 
+        public string SetupTokenFactory()
+        {
+
+        }
     }
 }
