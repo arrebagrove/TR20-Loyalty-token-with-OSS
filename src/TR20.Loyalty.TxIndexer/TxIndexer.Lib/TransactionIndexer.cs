006f3d7b939672883eda4f3d9b324b04a2388f2d
DB Lee <donlee@microsoft.com>
2019-06-21 12:37:42 +0900

    update Transaction Indexer

--- src/TR20.Loyalty.TxIndexer/TxIndexer.Lib/TransactionIndexer.cs
+++ src/TR20.Loyalty.TxIndexer/TxIndexer.Lib/TransactionIndexer.cs
@@ -22,59 +22,15 @@ namespace TR20.Loyalty.TxIndexer.Library
               new BusinessTransactionRepository<ContractTransaction, Guid>(new MongoClient(mongoConnectionString));
         }
 
-        //for Indexer
-        //public async Task<bool> LogTransaction(ContractTransaction transactionInformation)
-        //{
-        //    await LogTransaction(transactionInformation);
-
-        //    return true;
-        //}
-
         public async Task<bool> LogTransaction(ContractTransaction transactionInformation, string description = null)
         {
-            //Marking previous tx as inactive
-            //var previousTxs =
-            //    _transactionIndexRepository.FindAll(new GenericSpecification<ContractTransaction>(x => x.BusinessContractDTO.CitizenIdentifier == transactionInformation.BusinessContractDTO.CitizenIdentifier));
-            //foreach (var item in previousTxs)
-            //{
-            //    if (item.IsActiveTransaction)
-            //    {
-            //        item.IsActiveTransaction = false;
-            //        await _transactionIndexRepository.SaveAsync(item);
-            //    }
-
-            //}
-
             if (!string.IsNullOrEmpty(description))
             {
                 transactionInformation.BusinessContractDTO.Description = description;
             }
 
-            //transactionInformation.IsActiveTransaction = true;
             await _transactionIndexRepository.SaveAsync(transactionInformation);
-
             return true;
         }
-
-        //for Indexer
-        //public async Task<bool> UpdateTransaction(ContractTransaction transactionInformation)
-        //{
-        //    await _transactionIndexRepository.SaveAsync(transactionInformation);
-
-        //    return true;
-        //}
-
-        //public async Task<string> CreateProfile(string citizenName, string address, DateTime DateOfBirth, bool Citizenship, int FederalIncome, int StateIncome)
-        //{
-        //    var _simulatedDeployConfirmation = prepareDeployObject();
-        //    TransactionIndexingServiceAgent txAgent = new TransactionIndexingServiceAgent(_mongoConnectionString);
-        //    var result = await txAgent.LogTransaction(_simulatedDeployConfirmation);
-
-        //    return _simulatedDeployConfirmation.BindingId; 
-        //    //$"The ESC was deployed successfully, BindingId is : {_simulatedDeployConfirmation.BindingId}"; //result.ToString();
-        //}
-
-
-
     }
 }
